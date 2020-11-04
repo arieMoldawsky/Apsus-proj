f0a91d1e45b79c90f39407fd8abf88c1b8892408
Arie Moldawsky <ariemol555@gmail.com>
2020-11-04 19:26:21 +0200

    rearranged folders

--- js/routes.js
+++ js/routes.js
@@ -1,11 +1,11 @@
-import homePage from '../pages/home-page-cmp.js';
-import keepApp from '../apps/keep/keep-app.js';
+import homePage from './pages/home-page-cmp.js';
+import keepApp from './apps/keep/keep-app.js';
 // import about from './pages/about.cmp.js';
-import mailApp from '../apps/mail/mail-app.cmp.js';
-import mailList from '../apps/mail/cmps/mail-list.cmp.js';
-import mailDetails from '../apps/mail/pages/mail-details.cmp.js';
-import mailCompose from '../apps/mail/pages/mail-compose.cmp.js';
-// import keepDetails from '../apps/keep/keep-details.cmp.js';
+import mailApp from './apps/mail/mail-app.cmp.js';
+import mailList from './apps/mail/cmps/mail-list.cmp.js';
+import mailDetails from './apps/mail/pages/mail-details.cmp.js';
+import mailCompose from './apps/mail/pages/mail-compose.cmp.js';
+// import keepDetails from './apps/keep/keep-details.cmp.js';
 // import bookApp from './pages/book-app.cmp.js';
 // import bookDetails from './pages/book-details.cmp.js';
 
