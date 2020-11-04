3eca63aab42da683a6dbbb3f46ef7411615281b3
Arie Moldawsky <ariemol555@gmail.com>
2020-11-04 10:43:26 +0200

    main App

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
+import { myRouter } from './routes.js';
+
 console.log('Hiiiiii');
+
+
+const options = {
+    el: '#app',
+    router: myRouter,
+    template: `
+        <section>
+            <!-- <app-header></app-header> -->
+            <!-- <router-view></router-view> -->
+        </section>
+    `,
+    components: {
+        // bookApp,
+        // homePage,
+        // appHeader
+    },
+}
+
+const app = new Vue(options);
---
+++ js/routes.js
@@ -0,0 +1,26 @@
+// import homePage from './pages/home-page.cmp.js';
+// import about from './pages/about.cmp.js';
+// import bookApp from './pages/book-app.cmp.js';
+// import bookDetails from './pages/book-details.cmp.js';
+
+
+const myRoutes = [
+    // {
+    //     path: '/',
+    //     component: homePage
+    // },
+    // {
+    //     path: '/about',
+    //     component: about
+    // },
+    // {
+    //     path: '/book',
+    //     component: bookApp
+    // },
+    // {
+    //     path: '/book/details/:bookId?',
+    //     component: bookDetails
+    // }
+]
+
+export const myRouter = new VueRouter({ routes: myRoutes });
