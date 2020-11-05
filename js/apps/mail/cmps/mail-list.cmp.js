76cdbbfe335e26ad292c30b932184a791d10608d
Arie Moldawsky <ariemol555@gmail.com>
2020-11-05 11:09:38 +0200

    modifications + sent items

--- js/apps/mail/cmps/mail-list.cmp.js
+++ js/apps/mail/cmps/mail-list.cmp.js
@@ -1,32 +1,46 @@
 import mailPreview from './mail-preview.cmp.js';
 
 export default {
-    props: ['inboxMails', 'isMailDetails'],
+    props: ['inboxMails', 'isMailDetails', 'sentMails'],
     template: `
     <section  class="mail-list-section">
         <ul class="inbox-mails-list">
-            <li class="inbox-mail" v-for="mail in inboxMails" :key="mail.id" @click="onMailClick(mail)"><mail-preview :mail="mail"/></li>
+            <li class="inbox-mail" v-for="mail in mailsToShow" :key="mail.id" @click="onMailClick(mail)"><mail-preview :isInbox="isInbox" :mail="mail"/></li>
         </ul>
+        <!-- <router-view><router-view/> -->
     </section>
 `,
-data() {
-    return {
-        showList: this.isMailDetails,
-    }
-},
-methods: {
-    onMailClick(mail) {
-        this.$emit('showDetails', this.showList)
-        this.showList = false;
-        mail.isRead = true;
-        this.$router.push(`/mail/details/${mail.id}`)
-        // console.log(mail);
-    }
+    data() {
+        return {
+            showList: this.isMailDetails,
+            mailsBoxList: []
+        }
+    },
+    methods: {
+        onMailClick(mail) {
+            this.$emit('showDetails', this.showList)
+            this.showList = false;
+            mail.isRead = true;
+            this.$router.push(`/mail/details/${mail.id}`);
+            // if (this.$route.path === '/mail/sent') this.$router.push(`/mail/sent/details/${mail.id}`);
+            // else this.$router.push(`/mail/inbox/details/${mail.id}`)
+            // console.log(mail);
+        },
+    },
+    computed: {
+        mailsToShow() {
+            if (this.$route.path === '/mail/sent') return this.sentMails;
+            else return this.inboxMails
+        },
+        isInbox() {
+            if (this.$route.path === '/mail/sent') return false;
+            else return true
+        }
 
-},
-created() {
-},
-components: {
-    mailPreview
-}
+    },
+    created() {
+    },
+    components: {
+        mailPreview
+    }
 }
--- js/apps/mail/pages/mail-details.cmp.js
+++ js/apps/mail/pages/mail-details.cmp.js
@@ -7,15 +7,16 @@ export default {
     <section v-show="showMailDetails" class="mail-details-section">
     <button @click="onBackToInbox">Go Back</button><button @click="onDeleteMail">Delete</button>
         <div class="details-subject">{{theMail.subject}}</div>
-        <div class="details-sender">From: {{theMail.sender}}</div>
+        <div class="details-sender" v-show="isInbox">From: {{theMail.sender}}</div>
+        <div class="details-sender" v-show="!isInbox">To: {{theMail.to}}</div>
         <div class="details-body">{{theMail.body}}</div>
     </section>
 `,
     data() {
         return {
             showMailDetails: !this.showDetails,
-            theMail: {},
-
+            theMail: {subject: '', sender: '', to: '', body: ''},
+            isInbox: true
         }
     },
     methods: {
@@ -37,6 +38,18 @@ export default {
     created() {
         const id = this.$route.params.mailId;
         mailService.getMailById(id)
-            .then(mail => this.theMail = mail)
+        .then(mail => {
+            if (!mail) {
+                mailService.getSentById(id)
+                    .then(mail => this.theMail = mail)
+                    .then(() => this.isInbox = false)
+            } else {
+                this.isInbox = true;
+                return mail;
+            }
+        })
+            .then(mail => {
+                if (mail) this.theMail = mail
+            })
     }
 }
