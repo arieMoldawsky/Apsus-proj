8e13684ce79add08f6d7f769d127f1ff4a1148e2
Arie Moldawsky <ariemol555@gmail.com>
2020-11-05 20:38:04 +0200

    main design

--- js/pages/apsus-header-cmp.js
+++ js/pages/apsus-header-cmp.js
@@ -2,11 +2,13 @@
 export default {
     template: `
     <section class="apsus-header">
-        <div>Logo-Apsus</div>
-        <router-link to="/">Home</router-link>
-        <router-link to="/mail/inbox">Mail</router-link>
-        <router-link to="/keep/regular">Keep</router-link>
-        <router-link to="/books">Books</router-link>
+        <div class="header-logo">Apsus<span>.</span></div>
+        <div class="head-nav">
+            <router-link to="/">Home</router-link>
+            <router-link to="/mail/inbox">Mail</router-link>
+            <router-link to="/keep/regular">Keep</router-link>
+            <router-link to="/books">Books</router-link>
+        </div>
     </section>
 `
 }
