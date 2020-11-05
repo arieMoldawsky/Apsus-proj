26d8f1ab9e4cd41267e649641ffb0df84d134164
Arie Moldawsky <ariemol555@gmail.com>
2020-11-05 17:04:38 +0200

    compose improvments

--- js/apps/mail/pages/mail-compose.cmp.js
+++ js/apps/mail/pages/mail-compose.cmp.js
@@ -1,4 +1,5 @@
 import { mailService } from '../services/mail-service.js';
+// import {eventBus, 'send-keep-as-mail',} from '../services/event-bus-service.js'
 
 
 export default {
@@ -6,20 +7,26 @@ export default {
     <!-- <section v-show="showMailCompose" class="mail-compose-section"> -->
     <section class="mail-compose-section">
         <div class="compose-title">New Message</div>
-        <div class="pre-input">To:</div><input type="text" placeholder="Recipents" v-model="composedMail.to"/>
-        <div class="pre-input">Cc:</div><input type="text" v-model="composedMail.copy"/>
-        <div class="pre-input">Bcc:</div><input type="text" v-model="composedMail.blindCopy"/>
-        <div class="pre-input">Subject:</div><input type="text" placeholder="The Subject" v-model="composedMail.subject"/>
-        <textarea cols="60" rows="20" v-model="composedMail.body"></textarea>
+        <label class="compose-to-container compose-container">
+            To: <input type="text" placeholder="Recipents" v-model="composedMail.to"/>
+        </label>
+        <label class="compose-copy-container compose-container">
+            Cc: <input type="text" v-model="composedMail.copy"/>
+        </label>
+        <label class="compose-bcopy-container compose-container">
+            Bcc: <input type="text" v-model="composedMail.blindCopy"/>
+        </label>
+        <label class="compose-subject-container compose-container">
+            <div class="pre-input">Subject:</div>
+            <input type="text" placeholder="The Subject" v-model="composedMail.subject"/>
+        </label>
+        <textarea cols="60" rows="18" v-model="composedMail.body"></textarea>
         <button class="send-mail-btn" @click="onSendMail">Send</button>
-        <pre>
-        {{composedMail}}
-        </pre>
     </section>
 `,
     data() {
         return {
-            composedMail: {to: '', copy: '', blindCopy: '', sender: 'Tal H.', subject: '', body: '', isRead: true, sentAt: Date.now(), id: ''}
+            composedMail: { to: '', copy: '', blindCopy: '', sender: 'Tal H.', subject: '', body: '', isRead: true, sentAt: Date.now(), id: '' }
 
         }
     },
@@ -40,5 +47,9 @@ export default {
         const id = this.$route.params.mailId;
         mailService.getMailById(id)
             .then(mail => this.theMail = mail)
+
+        // eventBus.$on('send-keep-as-mail', msg => {
+        //     this.composedMail = msg;
+        // })
     }
 }
